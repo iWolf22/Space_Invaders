399116ecef3e4dfae2ea5d0700b3f357c81b7e88
iWolf22 <74108689+iWolf22@users.noreply.github.com>
2022-06-24 23:26:40 -0400

    Final Version
    Moved all the pictures into a separate folder and made the intro screens a little longer!

--- Python SPACE INVADERS/Pygame_Space_Invaders.py
+++ Python SPACE INVADERS/Pygame_Space_Invaders.py
@@ -148,7 +148,7 @@ def victory():
     background1.create_background()
     draw.rect(window,(255,255,255),(100,300,200,100))
     draw.rect(window,(255,255,255),(400,300,200,100))
-    victory_score_text = Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Score: " + str(game_score),250,20,80)
+    victory_score_text = Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Score: " + str(game_score),250,20,80)
     for i in range(len(victory_text_list)):
         victory_text_list[i].draw_text()
     victory_score_text.draw_text()
@@ -216,72 +216,72 @@ window_height = 500
 display.set_caption("Space Invaders")
 window = display.set_mode((window_width,window_height))
 
-powerup_heart = GameSprite("Python SPACE INVADERS/power_up_icons_heart.png",450,200,100,0,0,"")
-powerup_blast = GameSprite("Python SPACE INVADERS/power_up_icons_blast.png",450,200,100,0,0,"")
-powerup_tech = GameSprite("Python SPACE INVADERS/power_up_icons_tech.png",450,200,100,0,0,"")
-powerup_battery = GameSprite("Python SPACE INVADERS/power_up_icons_battery.png",150,200,100,0,0,"")
-powerup_reload = GameSprite("Python SPACE INVADERS/power_up_icons_reload.png",150,200,100,0,0,"")
-powerup_speedup = GameSprite("Python SPACE INVADERS/power_up_icons_speedup.png",150,200,100,0,0,"")
-powerup_spike = GameSprite("Python SPACE INVADERS/power_up_icons_spike.png",150,200,100,0,0,"")
-powerup_wings = GameSprite("Python SPACE INVADERS/power_up_icons_wings.png",150,200,100,0,0,"")
-powerup_weight = GameSprite("Python SPACE INVADERS/power_up_icons_weight.png",350,200,100,0,0,"")
-powerup_eye = GameSprite("Python SPACE INVADERS/power_up_icons_eye.png",150,200,100,0,0,"")
+powerup_heart = GameSprite("Python SPACE INVADERS/images/power_up_icons_heart.png",450,200,100,0,0,"")
+powerup_blast = GameSprite("Python SPACE INVADERS/images/power_up_icons_blast.png",450,200,100,0,0,"")
+powerup_tech = GameSprite("Python SPACE INVADERS/images/power_up_icons_tech.png",450,200,100,0,0,"")
+powerup_battery = GameSprite("Python SPACE INVADERS/images/power_up_icons_battery.png",150,200,100,0,0,"")
+powerup_reload = GameSprite("Python SPACE INVADERS/images/power_up_icons_reload.png",150,200,100,0,0,"")
+powerup_speedup = GameSprite("Python SPACE INVADERS/images/power_up_icons_speedup.png",150,200,100,0,0,"")
+powerup_spike = GameSprite("Python SPACE INVADERS/images/power_up_icons_spike.png",150,200,100,0,0,"")
+powerup_wings = GameSprite("Python SPACE INVADERS/images/power_up_icons_wings.png",150,200,100,0,0,"")
+powerup_weight = GameSprite("Python SPACE INVADERS/images/power_up_icons_weight.png",350,200,100,0,0,"")
+powerup_eye = GameSprite("Python SPACE INVADERS/images/power_up_icons_eye.png",150,200,100,0,0,"")
 
-powerup_text = Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Power Up Options",140,50,75)
-powerup_text1 = Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Piercing",120,310,50)
-powerup_text2 = Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Bullets",145,360,50)
-powerup_text3 = Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Extra Bullet",410,335,45)
-powerup_text4 = Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Regeration",410,335,45)
-powerup_text5 = Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Extra Health",110,335,45)
+powerup_text = Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Power Up Options",140,50,75)
+powerup_text1 = Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Piercing",120,310,50)
+powerup_text2 = Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Bullets",145,360,50)
+powerup_text3 = Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Extra Bullet",410,335,45)
+powerup_text4 = Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Regeration",410,335,45)
+powerup_text5 = Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Extra Health",110,335,45)
 
 
 main_text_list = []
-main_text_list.append(Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Space Invaders Odyssey",40,50,75))
-main_text_list.append(Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Created By: Inferno Studios",450,5,25))
-main_text_list.append(Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Single",150,310,50))
-main_text_list.append(Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Player",145,360,50))
-main_text_list.append(Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Instructions",410,335,45))
+main_text_list.append(Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Space Invaders Odyssey",40,50,75))
+main_text_list.append(Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Created By: Inferno Studios",450,5,25))
+main_text_list.append(Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Single",150,310,50))
+main_text_list.append(Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Player",145,360,50))
+main_text_list.append(Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Instructions",410,335,45))
 
 inst_text_list = []
-inst_text_list.append(Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Back To",50,20,45))
-inst_text_list.append(Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Main Screen",20,70,45))
-inst_text_list.append(Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Instructions",250,20,100))
-inst_text_list.append(Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","In Space Invaders Odysssy you must",20,150,45))
-inst_text_list.append(Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","eliminate all of the invading aliens to",20,200,45))
-inst_text_list.append(Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","save mother earth. Use the arrow keys to",20,250,45))
-inst_text_list.append(Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","avoid incoming lazers and fire back using",20,300,45))
-inst_text_list.append(Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","the space bar. Best of luck!",20,350,45))
+inst_text_list.append(Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Back To",50,20,45))
+inst_text_list.append(Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Main Screen",20,70,45))
+inst_text_list.append(Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Instructions",250,20,100))
+inst_text_list.append(Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","In Space Invaders Odysssy you must",20,150,45))
+inst_text_list.append(Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","eliminate all of the invading aliens to",20,200,45))
+inst_text_list.append(Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","save mother earth. Use the arrow keys to",20,250,45))
+inst_text_list.append(Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","avoid incoming lazers and fire back using",20,300,45))
+inst_text_list.append(Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","the space bar. Best of luck!",20,350,45))
 
 victory_text_list = []
-victory_text_list.append(Phrase(0,255,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Victory",150,150,160))
-victory_text_list.append(Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Next Level",110,330,50))
-victory_text_list.append(Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Quit Game",410,330,50))
+victory_text_list.append(Phrase(0,255,0,"Authentic_Script_Rough[1].ttf","Victory",150,150,160))
+victory_text_list.append(Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Next Level",110,330,50))
+victory_text_list.append(Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Quit Game",410,330,50))
 
 gameover_text_list = []
-gameover_text_list.append(Phrase(255,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","GAMEOVER",150,150,110))
-gameover_text_list.append(Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Play Again",110,330,50))
-gameover_text_list.append(Phrase(0,0,0,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Quit Game",410,330,50))
+gameover_text_list.append(Phrase(255,0,0,"Authentic_Script_Rough[1].ttf","GAMEOVER",150,150,110))
+gameover_text_list.append(Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Play Again",110,330,50))
+gameover_text_list.append(Phrase(0,0,0,"Authentic_Script_Rough[1].ttf","Quit Game",410,330,50))
 
-text1 = Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Inferno Studios",50,50,100)
-text2 = Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Presents",50,250,100)
-text3 = Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Space Invaders",50,50,100)
-text4 = Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Odyssey",50,250,100)
-text6 = Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Press Space To Play",50,350,90)
+text1 = Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Inferno Studios",50,50,100)
+text2 = Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Presents",50,250,100)
+text3 = Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Space Invaders",50,50,100)
+text4 = Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Odyssey",50,250,100)
+text6 = Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Press Space To Play",50,350,90)
 
 
-big_player = Player("Python SPACE INVADERS/spaceinvadersship.png",450,250,200,10,50," ")
-player_flip = Player("Python SPACE INVADERS/spaceinvadersship_flip1.png",50,160,100,10,100," ")
+big_player = Player("Python SPACE INVADERS/images/spaceinvadersship.png",450,250,200,10,50," ")
+player_flip = Player("Python SPACE INVADERS/images/spaceinvadersship_flip1.png",50,160,100,10,100," ")
 
-background1 = Background("Python SPACE INVADERS/space_background.png",0,0,700,500)
+background1 = Background("Python SPACE INVADERS/images/space_background.png",0,0,700,500)
 
 badguy_list = []
 for i in range(10):
-    badguy_list.append(Enemy("Python SPACE INVADERS/pink_dude-png.png",i*40 + 300,160,25,4,100,"left"))
-    badguy_list.append(Enemy("Python SPACE INVADERS/orange_dude-png.png",i*40 + 300,185,25,4,100,"right"))
-    badguy_list.append(Enemy("Python SPACE INVADERS/red_dude-png.png",i*40 + 300,210,25,4,100,"left"))
-    badguy_list.append(Enemy("Python SPACE INVADERS/green_dude-png.png",i*40 + 300,235,25,4,100,"right"))
+    badguy_list.append(Enemy("Python SPACE INVADERS/images/pink_dude-png.png",i*40 + 300,160,25,4,100,"left"))
+    badguy_list.append(Enemy("Python SPACE INVADERS/images/orange_dude-png.png",i*40 + 300,185,25,4,100,"right"))
+    badguy_list.append(Enemy("Python SPACE INVADERS/images/red_dude-png.png",i*40 + 300,210,25,4,100,"left"))
+    badguy_list.append(Enemy("Python SPACE INVADERS/images/green_dude-png.png",i*40 + 300,235,25,4,100,"right"))
 
-big_badguy = Enemy("Python SPACE INVADERS/orange_dude-png.png",450,250,200,4,100,"right")
+big_badguy = Enemy("Python SPACE INVADERS/images/orange_dude-png.png",450,250,200,4,100,"right")
 
 power_option = 1
 game_score = 0
@@ -295,14 +295,14 @@ text1.draw_text()
 text2.draw_text()
 big_player.reset()
 display.update()
-time.delay(1000)
+time.delay(3000)
 
 background1.create_background()
 text3.draw_text()
 text4.draw_text()
 big_badguy.reset()
 display.update()
-time.delay(1000)
+time.delay(3000)
 
 
 in_game = False
@@ -310,7 +310,7 @@ in_game = False
 gameloop = True
 while gameloop == True:
 
-    player = Player("Python SPACE INVADERS/spaceinvadersship.png",325,420,50,10,1000 + extra_health," ")
+    player = Player("Python SPACE INVADERS/images/spaceinvadersship.png",325,420,50,10,1000 + extra_health," ")
 
     bullet_list = []
 
@@ -319,33 +319,33 @@ while gameloop == True:
     enemy_list = []
     for i in range(17):
         if level >= 1:
-            enemy_list.append(Enemy("Python SPACE INVADERS/pink_dude-png.png",i*40,10,25,8,100,"left"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/pink_dude-png.png",i*40,10,25,8,100,"left"))
         if level >= 2:
-            enemy_list.append(Enemy("Python SPACE INVADERS/orange_dude-png.png",i*40 + 25,35,25,8,100,"right"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/orange_dude-png.png",i*40 + 25,35,25,8,100,"right"))
         if level >= 3:
-            enemy_list.append(Enemy("Python SPACE INVADERS/red_dude-png.png",i*40,60,25,8,100,"left"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/red_dude-png.png",i*40,60,25,8,100,"left"))
         if level >= 4:
-            enemy_list.append(Enemy("Python SPACE INVADERS/green_dude-png.png",i*40 + 25,85,25,8,100,"right"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/green_dude-png.png",i*40 + 25,85,25,8,100,"right"))
         if level >= 5:
-            enemy_list.append(Enemy("Python SPACE INVADERS/purple_dude-png.png",i*40,110,25,8,100,"left"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/purple_dude-png.png",i*40,110,25,8,100,"left"))
         if level >= 6:
-            enemy_list.append(Enemy("Python SPACE INVADERS/light_blue_dude-png.png",i*40 + 25,135,25,8,100,"right"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/light_blue_dude-png.png",i*40 + 25,135,25,8,100,"right"))
         if level >= 7:
-            enemy_list.append(Enemy("Python SPACE INVADERS/yellow_dude-png.png",i*40,160,25,8,100,"left"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/yellow_dude-png.png",i*40,160,25,8,100,"left"))
         if level >= 8:
-            enemy_list.append(Enemy("Python SPACE INVADERS/orange_dude-png.png",i*40,10,25,8,100,"left"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/orange_dude-png.png",i*40,10,25,8,100,"left"))
         if level >= 9:
-            enemy_list.append(Enemy("Python SPACE INVADERS/red_dude-png.png",i*40 + 25,35,25,8,100,"right"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/red_dude-png.png",i*40 + 25,35,25,8,100,"right"))
         if level >= 10:
-            enemy_list.append(Enemy("Python SPACE INVADERS/green_dude-png.png",i*40,60,25,8,100,"left"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/green_dude-png.png",i*40,60,25,8,100,"left"))
         if level >= 11:
-            enemy_list.append(Enemy("Python SPACE INVADERS/purple_dude-png.png",i*40 + 25,85,25,8,100,"right"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/purple_dude-png.png",i*40 + 25,85,25,8,100,"right"))
         if level >= 12:
-            enemy_list.append(Enemy("Python SPACE INVADERS/light_blue_dude-png.png",i*40,110,25,8,100,"left"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/light_blue_dude-png.png",i*40,110,25,8,100,"left"))
         if level >= 13:
-            enemy_list.append(Enemy("Python SPACE INVADERS/yellow_dude-png.png",i*40 + 25,135,25,8,100,"right"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/yellow_dude-png.png",i*40 + 25,135,25,8,100,"right"))
         if level >= 14:
-            enemy_list.append(Enemy("Python SPACE INVADERS/pink_dude-png.png",i*40,160,25,8,100,"left"))
+            enemy_list.append(Enemy("Python SPACE INVADERS/images/pink_dude-png.png",i*40,160,25,8,100,"left"))
 
     time1 = 0
     time2 = 0
@@ -362,7 +362,7 @@ while gameloop == True:
         power_option = 2
 
     background1.create_background()
-    text5 = Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Level " + str(level),100,150,200)
+    text5 = Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Level " + str(level),100,150,200)
     text5.draw_text()
     text6.draw_text()
     display.update()
@@ -381,8 +381,8 @@ while gameloop == True:
 
         background1.create_background()
 
-        text_score = Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Score: " + str(game_score),520,450,50)
-        text_health = Phrase(255,255,255,"C:/Users/joshu/AppData/Local/Microsoft/Windows/INetCache/IE/9EP3SYSI/Authentic_Script_Rough[1].ttf","Health: " + str(player.player_health),20,450,50)
+        text_score = Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Score: " + str(game_score),520,450,50)
+        text_health = Phrase(255,255,255,"Authentic_Script_Rough[1].ttf","Health: " + str(player.player_health),20,450,50)
         text_score.draw_text()
         text_health.draw_text()
 
@@ -394,13 +394,13 @@ while gameloop == True:
             keys = key.get_pressed()
             if keys[K_SPACE] and time.get_ticks() >= time1:
                 for i in range(number_bullets):
-                    bullet_list.append(Bullet("Python SPACE INVADERS/green_lazer-png.png",player.rect.x - number_bullets * 5 + 10 * i + 7,player.rect.y + 10,5,10,20,bullet_health,500))
+                    bullet_list.append(Bullet("Python SPACE INVADERS/images/green_lazer-png.png",player.rect.x - number_bullets * 5 + 10 * i + 7,player.rect.y + 10,5,10,20,bullet_health,500))
                 time1 = time.get_ticks() + bullet_list[0].fire_delay
         
 
         if time.get_ticks() >= time2:
             random_number = random.randrange(0,(len(enemy_list)))
-            enemy_bullet_list.append(Bullet("Python SPACE INVADERS/red_lazar-png.png",enemy_list[random_number].rect.x,enemy_list[random_number].rect.y,10,20,20,5,500))
+            enemy_bullet_list.append(Bullet("Python SPACE INVADERS/images/red_lazar-png.png",enemy_list[random_number].rect.x,enemy_list[random_number].rect.y,10,20,20,5,500))
             time2 = time.get_ticks() + 2000 - len(enemy_list) * 10
 
         counter = 0
